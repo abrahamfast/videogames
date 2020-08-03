7b56c980b432f0bb7100c140d494a8b4f78156b3
abraham ghaemi <ig.ghaemi@gmail.com>
2020-08-04 00:22:30 +0430

    Style the Index Page with Tailwind CSS

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,1 +1,136 @@
 @extends('layouts.app')
+
+@section('content')
+
+	<div class="container mx-auto px-4">
+		<h2 class="text-blue-500 uppercase tracking-wide font-semibold">Popular GAMES</h2>
+		<div class="popular-games text-sm grid grid-cols-6 gap-12 border-t border-gray-800 pb-16">
+			<div class="game mt-8">
+				<div class="relative inline-block">
+					<a href="#">
+						<img src="/img/gg1.jpg" alt="game cover" class="hover:opacity-75 transition ease-in-out duration-150">
+					</a>
+				<div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full progress-list-item">
+					<div class="font-semibold text-xs flex justify-center items-center h-full">
+						800% 
+					</div>
+				</div>
+			</div>
+				<a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">Final Fantasy & remak</a>
+				<div class="text-gray-400 mt-1">Playstation4</div>
+			</div>
+			<div class="game mt-8">
+				<div class="relative inline-block">
+					<a href="#">
+						<img src="/img/gg1.jpg" alt="game cover" class="hover:opacity-75 transition ease-in-out duration-150">
+					</a>
+				<div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full progress-list-item">
+					<div class="font-semibold text-xs flex justify-center items-center h-full">
+						800% 
+					</div>
+				</div>
+			</div>
+				<a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">Final Fantasy & remak</a>
+				<div class="text-gray-400 mt-1">Playstation4</div>
+			</div>
+			<div class="game mt-8">
+				<div class="relative inline-block">
+					<a href="#">
+						<img src="/img/gg1.jpg" alt="game cover" class="hover:opacity-75 transition ease-in-out duration-150">
+					</a>
+				<div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full progress-list-item">
+					<div class="font-semibold text-xs flex justify-center items-center h-full">
+						800% 
+					</div>
+				</div>
+			</div>
+				<a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">Final Fantasy & remak</a>
+				<div class="text-gray-400 mt-1">Playstation4</div>
+			</div>
+			<div class="game mt-8">
+				<div class="relative inline-block">
+					<a href="#">
+						<img src="/img/gg1.jpg" alt="game cover" class="hover:opacity-75 transition ease-in-out duration-150">
+					</a>
+				<div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full progress-list-item">
+					<div class="font-semibold text-xs flex justify-center items-center h-full">
+						800% 
+					</div>
+				</div>
+			</div>
+				<a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">Final Fantasy & remak</a>
+				<div class="text-gray-400 mt-1">Playstation4</div>
+			</div>
+			<div class="game mt-8">
+				<div class="relative inline-block">
+					<a href="#">
+						<img src="/img/gg1.jpg" alt="game cover" class="hover:opacity-75 transition ease-in-out duration-150">
+					</a>
+				<div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full progress-list-item">
+					<div class="font-semibold text-xs flex justify-center items-center h-full">
+						800% 
+					</div>
+				</div>
+			</div>
+				<a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">Final Fantasy & remak</a>
+				<div class="text-gray-400 mt-1">Playstation4</div>
+			</div>
+			<div class="game mt-8">
+				<div class="relative inline-block">
+					<a href="#">
+						<img src="/img/gg1.jpg" alt="game cover" class="hover:opacity-75 transition ease-in-out duration-150">
+					</a>
+				<div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full progress-list-item">
+					<div class="font-semibold text-xs flex justify-center items-center h-full">
+						800% 
+					</div>
+				</div>
+			</div>
+				<a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">Final Fantasy & remak</a>
+				<div class="text-gray-400 mt-1">Playstation4</div>
+			</div>	
+		</div>
+	
+		<div class="flex my-10">
+			<div class="recently-reviewed w-3/4 mr-32">
+				<h2 class="text-blue-500 uppercase tracking-wide font-semibold">Recently Reviewed</h2>
+				<div class="recently-reviewd-container space-y-12 mt-8">
+					<div class="game bg-gray-800 rounded-lg shadow-md flex px-6 py-6">
+						<div class="relative inline-none">
+							<a href="#">
+								<img src="/img/gg1.jpg" alt="game cover" class="hover:opacity-75 transition ease-in-out duration-150">
+							</a>
+							<div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full progress-list-item">
+								<div class="font-semibold text-xs flex justify-center items-center h-full">
+									80% 
+								</div>
+							</div>
+						</div>
+						<div class="ml-12">
+							<a href="#" class="block text-lg font-semibold leading-tight hover:text-gray-400 mt-4">Final Fantasy & remak</a>
+							<div class="text-gray-400 mt-1">Playstation 4</div>
+							<p class="mt-6 text-gray-400">
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam dignissimos repellendus sit voluptatum, aliquam praesentium amet cupiditate, quis id perferendis iusto libero eligendi repellat, facere provident placeat. Repellat numquam nobis voluptatem quasi vel mollitia corrupti a, illum incidunt eaque pariatur.
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="most-anticipated w-1/4">
+				<h2 class="text-blue-500 uppercase tracking-wide font-semibold">Recently Reviewed</h2>
+				<div class="most-anticipated-container space-y-10 mt-8">
+					<div class="game flex">
+						<a href="#">
+							<img src="/img/gg1.jpg" alt="game cover" class="w-16 hover:opacity-75 transition ease-in-out duration-150">
+						</a>
+
+						<div class="ml-4">
+							<a href="" class="hover:text-gray-300">Cyberpunk 2077</a>
+							<div class="text-gray-400 text-sm 1">Sept 16, 2020</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div> 
+	</div>
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,10 @@
 			</div>
 			<div class="flex items-center">
 				<div class="relative">
-					<input type="text" class="bg-gray-800 text-sm rounded-full px-3 py-1" placeholder="Search">
+					<input type="text" class="bg-gray-800 text-sm rounded-full w-64 px-3 pl-8 py-1 focus:outline-none focus:shadow" placeholder="Search">
+					<div class="absolute top-0 flex items-center h-full ml-2">
+						<svg class="fill-current text-gray-400 w-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24"><path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0016 9.5 6.5 6.5 0 109.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/><path d="M0 0h24v24H0z" fill="none"/></svg>
+					</div>
 				</div>
 				<div class="ml-6">
 					<a href="/">
@@ -31,6 +34,17 @@
 			</div>
 		</nav>
 	</header>
+
+
+	<main class="py-8">
+		@yield('content')
+		
+		<footer class="border-t border-gray-800">
+			<div class="container m1-auto px-4 py-6">
+				Powerb By <a href="#" class="underline hover:text-gray-400"></a>
+			</div>
+		</footer>
+	</main>
 	
 </body>
 </html>

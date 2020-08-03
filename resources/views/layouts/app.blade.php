059e33698df79f026f6d13e2bb82e38a7aa325f2
abraham ghaemi <ig.ghaemi@gmail.com>
2020-08-03 22:16:25 +0430

    added layouts with navbar

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Video Games</title>
+	<link rel="stylesheet" href="/css/main.css">
+</head>
+<body class="bg-gray-900 text-white">
+	<header class="border-b border-gray-800">
+		<nav class="container mx-auto flex items-center justify-between px-4 py-6">
+			<div class="flex items-center">
+				<a href="/">
+					<img src="/img/video-game.png" alt="videogames" class="w-32 flex-none">
+				</a>
+				<ul class="flex ml-24 space-x-8">
+					<li><a href="#" class="hover:text-gray-400">Games</a></li>
+					<li><a href="#" class="hover:text-gray-400">Reviews</a></li>
+					<li><a href="#" class="hover:text-gray-400">Coming</a></li>
+				</ul>
+			</div>
+			<div class="flex items-center">
+				<div class="relative">
+					<input type="text" class="bg-gray-800 text-sm rounded-full px-3 py-1" placeholder="Search">
+				</div>
+				<div class="ml-6">
+					<a href="/">
+						<img src="/img/avatar.jpg" alt="avatar" class="rounded-full w-10">
+					</a>
+				</div>
+			</div>
+		</nav>
+	</header>
+	
+</body>
+</html>

d378839903123cd00ede747dd431bf9dcee48a94
abraham ghaemi <ig.ghaemi@gmail.com>
2020-08-05 01:44:49 +0430

    Make the Index Page Responsive

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
 
 	<div class="container mx-auto px-4">
 		<h2 class="text-blue-500 uppercase tracking-wide font-semibold">Popular GAMES</h2>
-		<div class="popular-games text-sm grid grid-cols-6 gap-12 border-t border-gray-800 pb-16">
+		<div class="popular-games text-sm grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 xl:grid-6 gap-12 gap-12 border-t border-gray-800 pb-16">
 			<div class="game mt-8">
 				<div class="relative inline-block">
 					<a href="#">
@@ -12,7 +12,7 @@
 					</a>
 				<div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full progress-list-item">
 					<div class="font-semibold text-xs flex justify-center items-center h-full">
-						800% 
+						80% 
 					</div>
 				</div>
 			</div>
@@ -26,7 +26,7 @@
 					</a>
 				<div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full progress-list-item">
 					<div class="font-semibold text-xs flex justify-center items-center h-full">
-						800% 
+						80% 
 					</div>
 				</div>
 			</div>
@@ -40,7 +40,7 @@
 					</a>
 				<div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full progress-list-item">
 					<div class="font-semibold text-xs flex justify-center items-center h-full">
-						800% 
+						80% 
 					</div>
 				</div>
 			</div>
@@ -54,7 +54,7 @@
 					</a>
 				<div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full progress-list-item">
 					<div class="font-semibold text-xs flex justify-center items-center h-full">
-						800% 
+						80% 
 					</div>
 				</div>
 			</div>
@@ -68,7 +68,7 @@
 					</a>
 				<div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full progress-list-item">
 					<div class="font-semibold text-xs flex justify-center items-center h-full">
-						800% 
+						80% 
 					</div>
 				</div>
 			</div>
@@ -82,7 +82,7 @@
 					</a>
 				<div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full progress-list-item">
 					<div class="font-semibold text-xs flex justify-center items-center h-full">
-						800% 
+						80% 
 					</div>
 				</div>
 			</div>
@@ -91,8 +91,8 @@
 			</div>	
 		</div>
 	
-		<div class="flex my-10">
-			<div class="recently-reviewed w-3/4 mr-32">
+		<div class="flex flex-col lg:flex-row my-10">
+			<div class="recently-reviewed lg:w-3/4 mr-0 lg:mr-32">
 				<h2 class="text-blue-500 uppercase tracking-wide font-semibold">Recently Reviewed</h2>
 				<div class="recently-reviewd-container space-y-12 mt-8">
 					<div class="game bg-gray-800 rounded-lg shadow-md flex px-6 py-6">
@@ -109,21 +109,20 @@
 						<div class="ml-12">
 							<a href="#" class="block text-lg font-semibold leading-tight hover:text-gray-400 mt-4">Final Fantasy & remak</a>
 							<div class="text-gray-400 mt-1">Playstation 4</div>
-							<p class="mt-6 text-gray-400">
+							<p class="mt-6 text-gray-400 hidden lg:block">
 								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam dignissimos repellendus sit voluptatum, aliquam praesentium amet cupiditate, quis id perferendis iusto libero eligendi repellat, facere provident placeat. Repellat numquam nobis voluptatem quasi vel mollitia corrupti a, illum incidunt eaque pariatur.
 							</p>
 						</div>
 					</div>
 				</div>
 			</div>
-			<div class="most-anticipated w-1/4">
-				<h2 class="text-blue-500 uppercase tracking-wide font-semibold">Recently Reviewed</h2>
+			<div class="most-anticipated lg:w-1/4">
+				<h2 class="text-blue-500 uppercase tracking-wide font-semibold">Most Anticipated</h2>
 				<div class="most-anticipated-container space-y-10 mt-8">
 					<div class="game flex">
 						<a href="#">
 							<img src="/img/gg1.jpg" alt="game cover" class="w-16 hover:opacity-75 transition ease-in-out duration-150">
 						</a>
-
 						<div class="ml-4">
 							<a href="" class="hover:text-gray-300">Cyberpunk 2077</a>
 							<div class="text-gray-400 text-sm 1">Sept 16, 2020</div>

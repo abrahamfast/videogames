15a7719c12fb2d9a5c81c85cc9c52eab2a4e7a2e
abraham ghaemi <ig.ghaemi@gmail.com>
2020-08-06 22:22:41 +0430

    Fetch Game Information from IGTV

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/', 'GameController@index');
 
 
 Route::get('/show', function(){
